3fd1de4d983996bf6148ca3293585b5455786941
Maksym Syvash <maxim.sivash@gmail.com>
2023-10-05 22:51:40 +0300

    working on snail

--- Haskell/Lab1imgui/app/Image.hs
+++ Haskell/Lab1imgui/app/Image.hs
@@ -1,13 +1,23 @@
 module Image where
+import Numeric.Natural (Natural)
+-- import Graphics.Rendering.OpenGL (PolygonMode(Point))
 
-pascalSnail :: Floating a => a -> a -> a -> (a, a)
-pascalSnail theta a l =
-    let r = l - a * sin theta
-        x = (l + a * cos theta) * cos theta
-        y = (l + a * cos theta) * sin theta
-    in (x,y)
+data Point a = Point {x::a, y::a}
 
-plotPascalSnail
+type Plot a = [Point a]
+
+pascalSnail :: Floating a => a -> a -> a -> Point a
+pascalSnail a l theta =
+    let --r = l - a * sin theta
+        xx = (l + a * cos theta) * cos theta
+        yy = (l + a * cos theta) * sin theta
+    in Point {x=xx, y=yy}
+
+plotPascalSnail :: (Floating a, Enum a) => a -> a -> Natural -> Plot a
+plotPascalSnail a l size = map snail [0, step .. (2*pi)]
+    where  
+        snail = pascalSnail a l
+        step = 2.0 *  pi / realToFrac size
 
 -- data Parameters = Parameters 
 -- type TMatrix = [[Float]]
